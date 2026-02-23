a365dafdc46e33c3a3d631db55a81c39b98d417e
Keshav-Ag-11 <agarwalkeshav1107@gmail.com>
2026-02-23 14:31:32 +0530

    Added Blackjack game with logo

---
+++ Blackjack.py
@@ -0,0 +1,54 @@
+import random
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card=random.choice(cards)
+    return card
+comp = []
+human=[]
+for i in range (2):
+    comp.append(deal_card())
+    human.append(deal_card())
+c= sum(comp)
+h= sum(human)
+print(f"Your cards: {human}, current score: {h}")
+print(f"Computer's first card: {comp[0]}")
+while True:
+    choice = input("Type 'y' to get another card , type 'n' to pass")
+    if choice == 'n':
+        while c<17:
+            card = random.choice(cards)
+            comp.append(card)
+            c= sum(comp)
+        if c>21:
+            print("You win!")
+            print("Computer cards:", comp)
+            print("Computer score:", c)
+        elif c<21 and c>h:
+            print("Computer wins!")
+            print("Computer cards:", comp)
+            print("Computer score:", c)
+        elif c<21 and c<h:
+            print("You win")
+            print("Computer cards:", comp)
+            print("Computer score:", c)
+        elif c == h:
+            print("Draw")
+            print("Computer cards:", comp)
+            print("Computer score:", c)
+        break
+    elif choice == 'y':
+        card=random.choice(cards)
+        human.append(card)
+        h=sum(human)
+        print(f"Your cards: {human}, current score: {h}")
+        if h>21:
+            print("Computer wins!")
+            print("Computer cards:", comp)
+            print("Computer score:", c)
+            break
+
+
+
+
